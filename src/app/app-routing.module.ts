7f19918c28d2bbc389c77e83a5ee2b13042ee144
Rahul M <rahulmutyalkar@gmail.com>
2021-02-22 00:34:59 +0530

    change base url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
